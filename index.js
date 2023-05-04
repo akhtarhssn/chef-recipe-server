62ae6d3e8c0bb8e8f2172f8de37a0becfcac7dde
Wixden <watchonlytutorial@gmail.com>
2023-05-04 08:54:23 +0600

    Vercel deployment added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 const chef = require("./data/chef.json");
+const recipe = require("./data/recipe.json");
 
 app.use(cors());
 
@@ -14,6 +15,23 @@ app.get("/", (req, res) => {
 app.get("/chef", (req, res) => {
   console.log(chef);
   res.send(chef);
+});
+app.get("/recipe", (req, res) => {
+  console.log(recipe);
+  res.send(recipe);
+});
+
+app.get("/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedRecipe = recipe.find((n) => n.id === parseInt(id));
+  res.send(selectedRecipe);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chefRecipe = recipe.filter((n) => n.chef_id === parseInt(id));
+  res.send(chefRecipe);
 });
 
 app.listen(port, () => {
